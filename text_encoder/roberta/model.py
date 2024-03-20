849099eac9ea62add4d9c9ef171958fd91ac5e5b
Theo Hintz <thintz.smc@gmail.com>
2024-03-20 11:39:39 +0000

    Added MXBai model

--- text_encoder/roberta/model.py
+++ text_encoder/roberta/model.py
@@ -1,6 +1,12 @@
 import os
+from typing import Sequence, Dict
+
+
 import torch
-from transformers import RobertaModel, RobertaTokenizer
+import torch.nn.functional as F
+import numpy as np
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel, RobertaModel, RobertaTokenizer
 
 
 class RoBERTaModel:
@@ -35,3 +41,77 @@ class RoBERTaModel:
         elif pooling == "mean":
             pooled_output = embeddings.mean(dim=1)
         return pooled_output
+
+
+class SFREmbedding:
+    def __init__(self):
+        self.model = AutoModel.from_pretrained("Salesforce/SFR-Embedding-Mistral")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "Salesforce/SFR-Embedding-Mistral"
+        )
+
+        self.max_length = 4096
+
+    def last_token_pool(
+        self, last_hidden_states: Tensor, attention_mask: Tensor
+    ) -> Tensor:
+        left_padding = attention_mask[:, -1].sum() == attention_mask.shape[0]
+        if left_padding:
+            return last_hidden_states[:, -1]
+        else:
+            sequence_lengths = attention_mask.sum(dim=1) - 1
+            batch_size = last_hidden_states.shape[0]
+            return last_hidden_states[
+                torch.arange(batch_size, device=last_hidden_states.device),
+                sequence_lengths,
+            ]
+
+    def encode(self, texts: Sequence[str], l2_normalize: bool = True):
+        batch_dict = self.tokenizer(
+            texts,
+            max_length=self.max_length,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+        )
+        outputs = self.model(**batch_dict)
+        embeddings = self.last_token_pool(
+            outputs.last_hidden_state, batch_dict["attention_mask"]
+        )
+
+        if l2_normalize:
+            embeddings = F.normalize(embeddings, p=2, dim=1)
+
+        else:
+            return embeddings
+
+
+class MXBai:
+    def __init__(self):
+        self.model = AutoModel.from_pretrained("mixedbread-ai/mxbai-embed-large-v1")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "mixedbread-ai/mxbai-embed-large-v1"
+        )
+
+        self.max_length = 4096
+
+    # The model works really well with cls pooling (default) but also with mean poolin.
+    def pooling(
+        self, outputs: torch.Tensor, inputs: Dict, strategy: str = "cls"
+    ) -> np.ndarray:
+        if strategy == "cls":
+            outputs = outputs[:, 0]
+        elif strategy == "mean":
+            outputs = torch.sum(
+                outputs * inputs["attention_mask"][:, :, None], dim=1
+            ) / torch.sum(inputs["attention_mask"])
+        else:
+            raise NotImplementedError
+        return outputs.detach().cpu()
+
+    def encode(self, texts: Sequence[str]):
+        inputs = self.tokenizer(texts, padding=True, return_tensors="pt")
+        outputs = self.model(**inputs).last_hidden_state
+        embeddings = self.pooling(outputs, inputs, "cls")
+
+        return F.normalize(embeddings, p=2, dim=1)
